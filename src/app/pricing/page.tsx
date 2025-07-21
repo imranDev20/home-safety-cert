7f0ec6eff0d5f6132026c7ece13a507e0e470f8a
Imran Kabir <imrandev20@gmail.com>
2025-07-21 14:10:27 +0600

    Add pricing page vat text

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,34 +1,44 @@
-import { Box, Container } from "@mui/material";
-import React from "react";
-import PageHeader from "../_components/common/page-header";
 import BackgroundImage from "@/images/about-bg.jpeg";
-import PricingTable from "./_components/pricing-table";
+import { Box, Container, Typography } from "@mui/material";
+import React from "react";
 import Heading from "../_components/common/heading";
+import PageHeader from "../_components/common/page-header";
 import Paragraph from "../_components/common/paragraph";
+import PricingTable from "./_components/pricing-table";
 
 export default function PricingPage() {
-  return (
-    <Container maxWidth={false} disableGutters>
-      <PageHeader backgroundImage={BackgroundImage} title="Pricing" />
+	return (
+		<Container maxWidth={false} disableGutters>
+			<PageHeader backgroundImage={BackgroundImage} title="Pricing" />
 
-      <Box
-        sx={{
-          backgroundColor: "#F7F7F7",
-          py: 10,
-        }}
-      >
-        <Heading sx={{ textAlign: "center", mb: 1 }}>
-          Transparent Pricing for Peace of Mind
-        </Heading>
-        <Paragraph
-          sx={{ maxWidth: 700, mx: "auto", textAlign: "center", mb: 5 }}
-        >
-          Explore our pricing options for hassle-free landlord safety
-          certifications. We keep it simple and transparent, so you can focus on
-          property safety and compliance without any surprises.
-        </Paragraph>
-        <PricingTable />
-      </Box>
-    </Container>
-  );
+			<Box
+				sx={{
+					backgroundColor: "#F7F7F7",
+					py: 10,
+				}}
+			>
+				<Heading sx={{ textAlign: "center", mb: 1 }}>
+					Transparent Pricing for Peace of Mind
+				</Heading>
+
+				<Paragraph
+					sx={{ maxWidth: 700, mx: "auto", textAlign: "center", mb: 5 }}
+				>
+					Explore our pricing options for hassle-free landlord safety
+					certifications. We keep it simple and transparent, so you can focus on
+					property safety and compliance without any surprises.
+				</Paragraph>
+
+				<Typography
+					sx={{
+						mb: 5,
+						textAlign: "center",
+					}}
+				>
+					All prices are excluding VAT
+				</Typography>
+				<PricingTable />
+			</Box>
+		</Container>
+	);
 }
